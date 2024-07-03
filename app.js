b041a6e9ce309de1c2cf61f7403889c79cc5dcb1
Marrhero <Marrsadiq@gmail.com>
2024-07-03 17:43:27 +0100

    Major changes - ensured image won't scale, changed image path, design changes

--- app.js
+++ app.js
@@ -5,29 +5,29 @@ const timeText = document.getElementById("currentTime");
 //Set the date
 
 function updateTime(){
-const currentDate = new Date();
-const day = currentDate.getUTCDay();
+    const currentDate = new Date();
+    const day = currentDate.getUTCDay();
 
-const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-dayText.textContent = daysOfWeek[day];
+    dayText.textContent = daysOfWeek[day];
 
-var hours = currentDate.getUTCHours();
-var minutes = currentDate.getUTCMinutes();
-var seconds = currentDate.getUTCSeconds();
+    var hours = currentDate.getUTCHours();
+    var minutes = currentDate.getUTCMinutes();
+    var seconds = currentDate.getUTCSeconds();
 
-if (hours < 10)
-    hours = "0" + hours;
+    if (hours < 10)
+        hours = "0" + hours;
 
-if (minutes < 10)
-    minutes = "0" + minutes;
+    if (minutes < 10)
+        minutes = "0" + minutes;
 
-if (seconds < 10)
-    seconds = "0" + seconds;
+    if (seconds < 10)
+        seconds = "0" + seconds;
 
-//Set the time
-const currentTime = `${hours}:${minutes}:${seconds}`;
-timeText.textContent = currentTime;
+    //Set the time
+    const currentTime = `${hours}:${minutes}:${seconds}`;
+    timeText.textContent = currentTime;
 }
 
 setInterval(updateTime, 1000);
